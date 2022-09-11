0ca864797b4f9810e428425a828ae62e86921585
Evandrolds <evandro.lds.ds@outlook.com>
2022-09-11 20:28:58 -0300

    Projeto criado

---
+++ src/test/java/com/evandro/workshopspringboot/WorkshopSpringbootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evandro.workshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
